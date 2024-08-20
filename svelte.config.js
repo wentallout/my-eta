450f0010cf559683ce5b59f5d811875e29d30317
Nguyen Dang Khoa <wentallout@gmail.com>
2024-08-20 23:58:35 +0700

    use cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
